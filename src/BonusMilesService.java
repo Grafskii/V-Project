2d90aba25991fdf6024012994871468a455c5e96
Grafskii <miti-h@yandex.ru>
2023-04-23 22:27:53 +0300

    Initial commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int pricePerMile = 20; // цена за одну милю
+
+        return price / pricePerMile;
+    }
 }
